830a4c7691ff1906428a4cb2b5bc09befde3f81c
MohanKrishnaHB <mohankrishnahb@gmail.com>
2020-01-16 18:52:32 +0530

    added welcome page

--- charcha/urls.py
+++ charcha/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^healthcheck/$', views.health_check),
     url('', include('social_django.urls', namespace='social')),
+    url(r'^welcome/', views.welcome_page),
     url(r'^', include('charcha.discussions.urls')),
     url(r'^', include('charcha.pushpa.urls')),
     url(r'^', include('django.contrib.auth.urls')),
--- charcha/views.py
+++ charcha/views.py
@@ -1,5 +1,9 @@
 from __future__ import unicode_literals, absolute_import
 from django.http.response import HttpResponse
+from django.shortcuts import render
 
 def health_check(request):
     return HttpResponse("OK", status=200)
+
+def welcome_page(request):
+    return render(request, "welcome.html")
